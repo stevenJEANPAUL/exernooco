2f0f47d91be5391718779b498d942e7d08a7325e
steven <sacha.chadoes@gmail.com>
2022-06-06 23:29:21 +0200

    initialisation

---
+++ exerc/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bonjour à tous!!!
+        </p>
+        <button> cliquer ici</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
