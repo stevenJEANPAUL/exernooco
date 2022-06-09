2ef8f56fd0e0e87bd2b9b9b040cd914a79443c51
steven <sacha.chadoes@gmail.com>
2022-06-09 09:25:21 +0200

    maj data

--- exerc/src/App.js
+++ exerc/src/App.js
@@ -1,17 +1,47 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Bar, Doughnut} from "react-chartjs-2";
+import Chart from 'chart.js/auto';
+
+
+const data = {
+  labels:["Levis501", "Levis511", "GapSlim", "GapSkinny", "DieselSkinny", "DieselSlim"],
+  datasets: [
+    {
+      label:"Mes Articles",
+      data: ["99", "119", "99", "119", "129", "139"],
+      backgroundColor: ["#00ffff", "#7fffd4", "Red", "Violet", "Orange", "Yellow"],
+    },
+  ],
+}
+const data2 = {
+  labels:Array ["Red", "Blue", "Green", "Violet", "Orange", "Yellow"],
+  datasets: [
+    {
+      label:"mes artcles ",
+      data2: ["99", "119", "99", "119", "129", "139"],
+      produit:["99", "119", "99", "119", "129", "139"],
+      backgroundColor: ["Red", "Blue", "Green", "Violet", "Orange", "Yellow"],
+    },
+  ],
+}
+
+
+const options = {
+  maintainAspectRatio: false,
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bonjour à tous!!!
-        </p>
-        <button> cliquer ici</button>
-      </header>
+      <div className="container">
+      <div className="card2">
+        <Bar data={data} options={options} />
+      </div>
+      <div className="card">
+        <Doughnut data={data} options={options} />
+      </div>
+      </div>
     </div>
   );
 }
